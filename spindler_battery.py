64239b2b98665ee57afd0ee29236b33fe31a442d
harshityadav7 <74145806+harshityadav7@users.noreply.github.com>
2023-06-18 16:30:02 +0530

    Add files via upload

---
+++ spindler_battery.py
@@ -0,0 +1,14 @@
+from battery.battery import Battery
+from utils import add_years_to_date
+
+class SpindlerBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def need_service(self):
+        date_need_service = add_years_to_date(self.last_service_date, 4)
+        if date_need_service < self.current_date:
+            return True
+        else:
+            return False
